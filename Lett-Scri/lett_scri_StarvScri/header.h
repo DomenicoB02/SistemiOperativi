7fb84ddd62c3368f57bd9ae25bf1bc2bdba727bf
PacxeN <149240024+PacxeN@users.noreply.github.com>
2023-11-20 08:56:44 +0100

    Update header.h

--- Lett-Scri/lett_scri_StarvScri/header.h
+++ Lett-Scri/lett_scri_StarvScri/header.h
@@ -1,19 +1,13 @@
-		/*-------HEADER FILE-------------*/
-
 #define MUTEXL 0
 #define SYNCH 1
 #define NUM_VOLTE 6
 
-
 typedef long  msg;
 
 typedef struct {
-
         int numlettori;
         msg messaggio;
-    
 } Buffer;
-
 
 void Lettore(int,Buffer*);
 void Scrittore(int,Buffer*);
