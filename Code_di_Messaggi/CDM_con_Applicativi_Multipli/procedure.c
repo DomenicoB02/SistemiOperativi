5cdc38f42d83eb591d2ebdde68ff9131f2125869
PacxeN <149240024+PacxeN@users.noreply.github.com>
2023-11-20 17:15:37 +0100

    Add files via upload

---
+++ Code_di_Messaggi/CDM_con_Applicativi_Multipli/procedure.c
@@ -0,0 +1,9 @@
+#include "procedure.h"
+
+#include <time.h>
+#include <stdlib.h>
+
+float generaFloat(int i_sx, int i_dx){
+    float x=(float)((rand()+i_sx)/(float)(RAND_MAX/i_dx));
+    return x;
+}
