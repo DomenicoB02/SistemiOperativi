d5b1fd567a80e0a00afeea783d60056e72a1374c
PacxeN <149240024+PacxeN@users.noreply.github.com>
2023-11-20 17:14:39 +0100

    Delete Code_di_Messaggi/CDM_con_Applicativi_Multipli directory

--- Code_di_Messaggi/CDM_con_Applicativi_Multipli/procedure.c
+++
@@ -1,9 +0,0 @@
-#include "procedure.h"
-
-#include <time.h>
-#include <stdlib.h>
-
-float generaFloat(int i_sx, int i_dx){
-    float x=(float)((rand()+i_sx)/(float)(RAND_MAX/i_dx));
-    return x;
-}
