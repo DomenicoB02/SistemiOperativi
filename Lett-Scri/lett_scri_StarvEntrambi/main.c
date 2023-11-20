c11c97cb81dd22e5947acb65557e4c684f256594
PacxeN <149240024+PacxeN@users.noreply.github.com>
2023-11-20 09:23:15 +0100

    Update main.c

--- Lett-Scri/lett_scri_StarvEntrambi/main.c
+++ Lett-Scri/lett_scri_StarvEntrambi/main.c
@@ -26,9 +26,7 @@
 #include "header.h"
 
 int main(){
-
         /************DICHIARAZIONE DELLE VARIABILI***************/
-
         int id_sem, id_shared, k;
         key_t chiave = IPC_PRIVATE;
         key_t c_sem = IPC_PRIVATE;
@@ -37,9 +35,7 @@ int main(){
 
         pid_t pid;
         int num_processi = 10;
-
         /************RICHIESTA DEL SEGMENTO DI MEMORIA CONDIVISA***********/
-
 
         id_shared=shmget(chiave, sizeof(Buffer), IPC_CREAT | 0664);
         printf("id_shared=%d \n", id_shared);
@@ -50,7 +46,6 @@ int main(){
         ptr_sh->numscrittori = 0;
         ptr_sh->messaggio = 0;	
 
-
         //   richiesta dei semafori
         id_sem=semget(c_sem, 4, IPC_CREAT | 0664);
 
@@ -60,30 +55,21 @@ int main(){
         semctl(id_sem, MUTEXS, SETVAL, 1);
         semctl(id_sem, MUTEX, SETVAL, 1);
 
-
-
         /****GENERAZIONE DEI PROCESSI E OPERAZIONI DI R/W****/
-
-
         //generazione di scrittori e lettori
         for (k=0; k<num_processi; k++) {
-
                 pid=fork();
-
                 if (pid == 0)  {                //processo figlio
                         if ((k%2) == 0) {
-        
                                 printf("sono il figlio scrittore. Il mio pid %d \n", getpid());
                                 Scrittore(id_sem, ptr_sh);
                         }else {
-        
                                 printf("sono il figlio lettore. Il mio pid %d \n", getpid());
                                 Lettore(id_sem, ptr_sh);
                         } 
                         exit(0);
                 } 
         }
-
 
         for (k=0; k<num_processi; k++){
                 pid = wait(&status);
@@ -97,5 +83,4 @@ int main(){
         semctl(id_sem, 0, IPC_RMID);
 
         return 1;
-
 }
