e4f1fe02731c7b13d2eab6bdf30df46d22a65d53
PacxeN <149240024+PacxeN@users.noreply.github.com>
2023-11-22 15:26:31 +0100

    Update procedure.h

--- Monitor/ProdCons_SingoloBuffer_MonitorHoare/procedure.h
+++ Monitor/ProdCons_SingoloBuffer_MonitorHoare/procedure.h
@@ -1,15 +1,11 @@
 #ifndef HEADER_H
 #define HEADER_H
-
 #include "monitor_hoare.h"
-
 
 struct ProdCons {
     int buffer;
-
     int buffer_libero; // 1 se libero, 0 se pieno
     int buffer_occupato; // 0 se libero, 1 se pieno
-
     Monitor m;
 };
 
