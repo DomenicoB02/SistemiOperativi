be74cd6b264ddc3a1c0bf0220cf0fb74cc3d0c66
PacxeN <149240024+PacxeN@users.noreply.github.com>
2023-12-02 14:27:35 +0100

    Add files via upload

---
+++ Compito-SO-2023-01-31/publisher.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/msg.h>
+#include <time.h>
+
+#include "pubsub.h"
+
+void publisher(int id_coda_messaggi, long topic);
+
+int main() {
+
+    pid_t pid;
+    /* TBD */
+    key_t chiave_coda_messaggi = ftok(".",'B');
+    /* TBD: Creare una coda di messaggi dei publisher */
+    int id_coda_messaggi = msgget(chiave_coda_messaggi,IPC_CREAT|0664);
+
+    for(int i=0; i<NUM_PUBLISHERS_PER_TOPIC; i++) {
+        printf("[PUBLISHER] Avvio processo publisher su topic 1\n");
+        /* TBD: 
+            Creare un processo figlio, che esegua la funzione "publisher".
+            Passare come parametri lo ID della coda messaggi, e il valore TOPIC1.
+        */
+        pid=fork();
+        if(pid==0){
+            publisher(id_coda_messaggi,TOPIC1);
+            exit(0);
+        }
+    }
+
+    for(int i=0; i<NUM_PUBLISHERS_PER_TOPIC; i++) {
+
+        printf("[PUBLISHER] Avvio processo publisher su topic 2\n");
+
+        /* TBD: 
+            Creare un processo figlio, che esegua la funzione "publisher".
+            Passare come parametri lo ID della coda messaggi, e il valore TOPIC2.
+        */
+       pid=fork();
+       if(pid==0){
+        publisher(id_coda_messaggi,TOPIC2);
+        exit(0);
+       }
+    }
+    printf("[PUBLISHER] Attesa terminazione processi figli\n");
+
+    /* TBD: Attendere la terminazione dei processi figli */
+    for(int i=0;i<NUM_PUBLISHERS_PER_TOPIC*2;i++){
+        wait(NULL);
+    }
+}
+
+void publisher(int id_coda_messaggi, long topic) {
+
+    srand(time(NULL)*getpid());
+    for(int i=0; i<NUM_MESSAGGI; i++) {
+
+        int valore = rand() % 100;
+
+        messaggio_valore messaggio;
+
+        /* TBD:
+            Inviare un messaggio al processo broker.
+            Includere il topic ed il valore.
+        */
+        messaggio.topic=topic;
+        messaggio.valore=valore;
+        int ret=msgsnd(id_coda_messaggi,&messaggio,sizeof(messaggio_valore)-sizeof(long),0);
+        if(ret<0){
+            perror("Errore msgsn");
+            exit(1);
+        }
+
+        printf("[PUBLISHER] Messaggio inviato: topic=%ld, valore=%d\n", topic, valore);
+
+        sleep(1);
+    }
+}
+
---
+++ Compito-SO-2023-01-31/pubsub.h
@@ -0,0 +1,37 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+#define TOPIC1 1
+#define TOPIC2 2
+
+#define NUM_PUBLISHERS_PER_TOPIC 1
+#define NUM_SUBSCRIBERS_PER_TOPIC 2
+
+#define NUM_MESSAGGI 3
+#define TOTALE_SUBSCRIBER (NUM_PUBLISHERS_PER_TOPIC * 2)
+#define TOTALE_MESSAGGI (NUM_PUBLISHERS_PER_TOPIC * 2 * NUM_MESSAGGI)
+
+typedef struct {
+
+    /* TBD: 
+        Definire la struttura del messaggio, 
+        includendo un topic (numerico)
+        e un valore (numerico)
+     */
+    long topic;
+    int valore;
+
+} messaggio_valore;
+
+typedef struct {
+
+    /* TBD: 
+        Definire la struttura del messaggio, 
+        includendo un topic (numerico)
+        e lo ID della coda su cui ricevere i messaggi
+     */
+    long topic;
+    int id_coda;
+} messaggio_registrazione;
+
+#endif
